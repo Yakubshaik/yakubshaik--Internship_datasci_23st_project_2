c77f0be5994ad4b2586d004ccc83572c731897e1
Yakubshaik <112190388+Yakubshaik@users.noreply.github.com>
2023-02-25 07:39:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.header("Welcome to the page!")
 st.subheader("A page using streamlit:sunglasses:")
 st.text("stream used to build this app in a very easy way.")
 
-df= pd.read_csv(r"C:\Users\Admin\Desktop\Yakub_ML\Parkinsondata.csv")
+df= pd.read_csv("C:\Users\Admin\Desktop\Yakub_ML\Parkinsondata.csv")
 
 st.dataframe(df)
