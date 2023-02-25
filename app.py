5d8a1b3810f72fed64b6229d44b080f7832acf6b
Yakubshaik <112190388+Yakubshaik@users.noreply.github.com>
2023-02-25 07:35:55 +0530

    Add files via upload
    added a data frame.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.header("Welcome to the page!")
 st.subheader("A page using streamlit:sunglasses:")
 st.text("stream used to build this app in a very easy way.")
+
+df= pd.read_csv(r"C:\Users\Admin\Desktop\Yakub_ML\Parkinsondata.csv")
+
+st.dataframe(df)
