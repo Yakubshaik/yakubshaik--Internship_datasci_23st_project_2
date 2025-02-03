49d3d66718613946ed6f627e73b4fbf4a1172f43
Yakubshaik <112190388+Yakubshaik@users.noreply.github.com>
2025-02-03 15:31:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 import pandas as pd
-import os
-
-#file_path= os.path(r"C:\\Users\\Admin\\Desktop\\Yakub_ML\\Parkinsondata.csv")
-
 
 st.header("Welcome to the page!")
 st.subheader("A page using streamlit:sunglasses:")
 st.text("stream used to build this app in a very easy way.")
 
-df= pd.read_csv("Parkinsondata.csv")
+df= pd.read_csv("Data for task.csv")
 
 st.dataframe(df)
